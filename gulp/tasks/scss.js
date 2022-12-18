90fb7c0b1b19ae393bd77a7bbb09039240d9f696
Vladislav_Fokycnuk099 <foxhaund99@gmail.com>
2022-12-18 22:17:37 +0300

    Обработка SASS/SCSS/CSS файлов.Processing of SASS/SCSS/CSS files.

---
+++ gulp/tasks/scss.js
@@ -0,0 +1,33 @@
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import rename from 'gulp-rename';
+import cleanCss from 'gulp-clean-css'; // сжатие CSS файла
+import webpcss from 'gulp-webpcss';//вывод WEBP изображения
+import autoprefixer from 'gulp-autoprefixer';// добавление вендорных префиксов 
+import groupCssMediaQueries from 'gulp-group-css-media-queries';// группировка медиа запросов
+const sass = gulpSass(dartSass);
+
+export const scss = () => {
+    return app.gulp.src(app.path.src.scss, {sourcemaps: true}) //отлов файла с ошибкой
+    .pipe(app.plugins.replace(/@img\//g, '../images/'))
+    .pipe(sass({
+        outputStyle: 'expanded'
+    }))
+    .pipe(groupCssMediaQueries())
+    .pipe(webpcss({
+        webpClass: ".webp", // если бразуер поддерживает webp, то по этому классу будет выводится изображение
+        noWebpClass: ".no-webp"
+    }))
+    .pipe(autoprefixer({
+        grid: true, // включение поддержки грид
+        overrideBrowserslist: ["last 3 versions"],// количество версий бразуера от новой
+        cascade:true
+    }))
+    // .pipe(app.gulp.dest(app.path.build.css)) не сжатый дубль файла стилей
+    .pipe(cleanCss())
+    .pipe(rename({
+        extname: ".min.css"
+    }))
+    .pipe(app.gulp.dest(app.path.build.css))//выгрузка в итоговую папку
+    .pipe(app.plugins.browsersync.stream()) // обновляем браузер
+}
--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ import gulp from "gulp";
 //импорт путей
 import {path} from "./gulp/config/path.js";
 //импорт общих плагинов
-import { plugins } from "./gulp/config/plugins.js";
+import {plugins} from "./gulp/config/plugins.js";
 
 // передаем значение в глобальную переменную
 global.app = {
@@ -16,14 +16,15 @@ import {copy} from "./gulp/tasks/copy.js";
 import {del} from "./gulp/tasks/reset.js";
 import {html} from "./gulp/tasks/html.js";
 import {server} from "./gulp/tasks/server.js";
-
+import {scss} from "./gulp/tasks/scss.js";
 //наблюдатель за изменениями в файлах
 function watcher(){
     gulp.watch(path.watch.files, copy);
     gulp.watch(path.watch.html, html);
+    gulp.watch(path.watch.scss, scss);
 }
 
-const mainTasks = gulp.parallel(copy, html);//параллельное выполнение нескольких задач
+const mainTasks = gulp.parallel(copy, html, scss);//параллельное выполнение нескольких задач
 
 //построение сценариев выполнения задач (продакшн и разработка)
 const dev = gulp.series(del, mainTasks, gulp.parallel(watcher, server));//метод выполняет задачи последовательно
